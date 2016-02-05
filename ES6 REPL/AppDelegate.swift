9247acfd9b48093526e7528b3aeb2fa828e61546
Priit Haamer <priit@fraktal.ee>
2016-02-05 10:39:20 +0200

    Initial Commit

---
+++ ES6 REPL/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ES6 REPL
+//
+//  Created by Priit Haamer on 05.02.16.
+//  Copyright © 2016 Priit Haamer. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ES6 REPL/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ES6 REPL
+//
+//  Created by Priit Haamer on 05.02.16.
+//  Copyright © 2016 Priit Haamer. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
